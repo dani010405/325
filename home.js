985c6e0eaca8b2ee2f5496e0102e13762b9f1a94
Daniel <azielarden@gmail.com>
2026-01-03 11:03:38 +0800

    Update home.js

--- home.js
+++ home.js
@@ -132,7 +132,34 @@ function setupWindow(btnId, winId) {
 setupWindow("messageBtn", "messageWindow");
 
 const galleryData = [
-  { img: "imgs/photojournals/0102-0326.jpeg"},
+  { img: "imgs/photojournals/0102-0326.jpeg", text: [
+    "January 2-3, 2026",
+    "Are you here?",
+    "If you are, I don’t know, you shouldn’t try read this journal entry. I translated it into a fictional language. So, I hope that’s enough to make this hard to decipher/understand.",
+    "Please, skip this.",
+    "It’s 12:31 AM on January 3rd. I am writing this, attaching a photo I took on the night of January 2nd. I went for a walk—a really long walk. It took me 3 hours, 5 minutes, and 37 seconds to go back and forth to our house. I covered 11.57 kilometers, exceeding my record by 0.05 kilometers.",
+    "Actually, my mom begged me to stop walking for three hours; she said an hour or an hour and a half is enough.",
+    "Yn, ziry doesn’t shifang.  Kostilus nyke don’t, tolī.  Pāsan there’s daor rational drīve, sepār emotional.  I’ve issare following ñuha emotions whenever nyke decide naejot gaomagon bisa activity.  I’ve issare feeling ojūdan, se nyke don’t gīmigon skoriot naejot blame ziry iā skorkydoso naejot control ziry.",
+    "really, nyke thought īlen getting sȳrkta mōrī jēdarī.  Nyke thought ēdan ñuha ābrar mirre figured hen, nyke knew skoros naejot gaomagon, īlen isse control—turns hen, īles mirre wishful otāpagon.  Nyke gūrēntan daorun rȳ mirre.",
+    "nyke geron kesrio syt iksan ojūdan, se sīr nyke ivestragī ñuha dekossa jikagon skoriot jaelzi naejot.  Se maybe, bisa iksis se mērī run nyke feel isse control hen syt sir.",
+    "sīr maybe that’s skoro syt gaoman ziry.",
+    "i’m sīr ojūdan bona bisa entry ēza iā complete junky iāragon.",
+    "ȳdragon nūmāzma: iāragon state, iksan daor writer.",
+    "iksan iā imposter isse mirre se ra iksan passionate nūmāzma.  Iksan neither iā artist nor iā tech-oriented issaros; iksan daorun rȳ mirre.  Nyke don’t gīmigon skoros iksan.  Mirre ñuha ābrar, i’ve issare parading hae someone else.  It’s funny, daor?",
+    "iksan sīr lacking isse jorrāelagon.  Hae iā riña, nyke would sagon iā rōvēgrie tala se iā overachiever.  Despite mirre ñuha potential, īlen limited ondoso maybe iā mijegon hen attention.  Ñuha muñar would va moriot commend ñuha valonqar kesrio syt pōnta thought īles talented.  “thought” kesrio syt nyke pendagon ziry hasn’t mirre rȳ mirre.  Evil, huh? nyke mazverdatan bē hating zirȳla.  Ziry jiōraton mirre se jorrāelagon nyke va moriot worked qopsa syt.  Tolvie jēda pōnta commend zirȳla, nyke would va moriot feel iā adere surge hen passion naejot gaomagon sȳrkta isse ñuha academics.  Se tolī, nyke found art, skore nyke thought would finally jiōragon nyke mirri tolī jorrāelagon kesrio syt it’s iā talent.",
+    "qogralbar, iksan ēdrugī hen bisa narrative.  Nyke don’t vēdros ñuha lēkia rȳ mirre.  Nyke regret se giez ābrar nyke jentan trying naejot mazverdagon ñuha muñar jorrāelagon zirȳla less.  Īles sīr fucking childish se daor olvie sȳz hen iā uēpkta mandia qilōni should’ve dohaertan zirȳla gūrēñagon ābrar.  It’s drēje, nyke don’t vēdros zirȳla, se i’m vaoreznuni.  Yn, nopāzma ! recently, nyke felt sīr envious.  Iā fucking disgusting feeling hen someone claiming naejot feel vaoreznuni.",
+    "ziry rūdan hen hen school.  Saelie jēda sir.  Skoros vēttan nyke feel envious iksin bona ñuha mom eptan zirȳla lo ziry jeldan naejot ūndegon iā therapist arlī.  Pāsagon ziry iā daor, nyke pendagon ziry wasn’t envy nyke felt.  Nyke pendagon nyke felt daorun diabolical va ñuha lēkia rȳ bona jēda.  Mayhap, nyke felt upset bona ñuha mom knows ziry jorrāelagon mēre se ēza daor idea gaoman tolī.  It’s sesīr funnier otāpagon bona i’m rȳ se age bona nyke should, nyke don’t know—maybe handle ñuha own expenses? lo nyke jorrāelagon iā therapist, skoro syt don’t nyke invest isse mēre?",
+    "nyke don’t sesīr gīmigon skoros i’m talking nūmāzma rȳ bisa point.  Iksi daor isse iāragon state, folks.  Nyke gīmigon i’m iēdrosa writing, yn hae everything nyke bardugon iksis lēda hen confusion se qrīdronnor.",
+    "qrugh, it’s bē possible i’m sepār distracting nykēla hen se thoughts hen ao.  Nyke might’ve vēttan another grave mistake.  Ziry could sagon bona skoros nyke gōntan iksin simply chickenshit.  Nyke shifang ao daor replying iā responding.  Yn ziry hurts ñuha bartos otāpagon nūmāzma skorkydoso iksā doing.  Nyke hope you’re daor reading bisa.",
+    "should mazeman bisa website ilagon? should nyke disappear hen aōha sight? skoros should gaoman?",
+    "nyke don’t gīmigon.  I’m doru-borto.  Nyke probably disgust ao. ",
+    "daor, nyke should keligon writing.  I’m writing nonsense.  Mirre hen these iksis verdagon mirre sense.  Mirre hen these issi irresponsible, unaccountable, se shameless.",
+    "iksan gonna pendagon bisa rȳ.  Aaaaaaaaaaaaaaaaaaaaaaaaaaaa, bisa iksis doru-borto se highly immoral se demanding syt iā perpetrator hae nyke.",
+    "bisa iksis tolī olvie, bisa iksis daor gentle.  Skorkydoso iksis bisa gentle? skoros se qogralbar iksin nyke otāpagon?",
+    "skoros iksin nyke otāpagon sir?",
+    "nyke should cease writing sir"
+    ]  
+  },
   { img: "imgs/photojournals/010126.jpeg", text: [
     "January 1, 2026",
     "It's the first day of the year, and I am writing this feeling defeated by my anxieties about what would happen if I finally send her this letter. But KC asked me what I’d prefer. Would I prefer my efforts to stay with me, or would I prefer it to reach its muse, even if she doesn't respond? I think that now, at least, it would reach her. So I will send her this.",
@@ -204,6 +231,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
 });
+
 
 
 
